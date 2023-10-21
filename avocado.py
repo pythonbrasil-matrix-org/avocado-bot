e10b147206d758abf148512f3fb90143d079c6c4
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2023-10-21 00:25:10 -0300

    ok

--- avocado.py
+++ avocado.py
@@ -4,8 +4,23 @@ import simplematrixbotlib as botlib
 
 import credentials
 
-creds = botlib.Creds(credentials.HOMESERVER,
-                     credentials.USERNAME,
-                     credentials.PASSWORD)
+creds = botlib.Creds(homeserver=credentials.HOMESERVER,
+                     username=credentials.USERNAME,
+                     password=credentials.PASSWORD,
+                     session_stored_file=credentials.SESSION_FILE)
 
-avocado = botlib.Bot(creds)
+config = botlib.Config()
+config.encryption_enabled = True
+config.emoji_verify = True
+config.ignore_unverified_devices = False
+config.join_on_invite = False
+config.store_path = credentials.CRYPTO_STORE
+
+avocado = botlib.Bot(creds=creds, config=config)
+
+@avocado.listener.on_message_event
+async def echo(room, message):
+    print(room, message)
+
+
+avocado.run()
