83eddcea36cb1dbb5301680b4e5be7a453362de7
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2023-10-21 00:00:15 -0300

    ok

--- avocado.py
+++ avocado.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+
+import simplematrixbotlib as botlib
+
+import credentials
+
+creds = botlib.Creds(credentials.HOMESERVER,
+                     credentials.USERNAME,
+                     credentials.PASSWORD)
+
+avocado = botlib.Bot(creds)
