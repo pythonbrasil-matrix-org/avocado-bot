32a027a0899938de37c55fb22dbacaaaf9df920f
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2023-10-23 07:31:51 -0300

    commit

--- avocado/configuration.py
+++ avocado/configuration.py
@@ -4,7 +4,7 @@ from . import botsecrets
 CONFIG = botlib.Config()
 CONFIG.allowlist = botsecrets.ALLOW_LIST
 CONFIG.encryption_enabled = True
-CONFIG.emoji_verify = False
-CONFIG.ignore_unverified_devices = False
+CONFIG.emoji_verify = True
+CONFIG.ignore_unverified_devices = True
 CONFIG.join_on_invite = False
 CONFIG.store_path = botsecrets.CRYPTO_STORE
