442e300c72dabfe2e83f9575dac6997ffc9a4bfc
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2023-10-23 15:20:12 -0300

    commit

--- avocado/credentials.py
+++ avocado/credentials.py
@@ -3,6 +3,7 @@ from . import botsecrets
 
 CREDS = botlib.Creds(homeserver=botsecrets.HOMESERVER,
                      username=botsecrets.USERNAME,
-                     password=botsecrets.PASSWORD,
-                     session_stored_file=botsecrets.SESSION_FILE)
+                     password=botsecrets.PASSWORD)
+                     #  password=botsecrets.PASSWORD,
+                     #  session_stored_file=botsecrets.SESSION_FILE)
 
