12c9b592dc9fc28958ad38d17aa9048b6185a383
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2023-10-23 04:21:49 -0300

    commit

--- avocado/__main__.py
+++ avocado/__main__.py
@@ -1,3 +1,11 @@
+from nio import RoomMessageText
+
+from . import register_command
+from . import COMMAND_REGISTRY
+
 from .bot import avocado
+
+for command_name, command_function in COMMAND_REGISTRY.items():
+    avocado.listener._registry.append([command_function, RoomMessageText])
 
 avocado.run()
