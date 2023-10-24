d8de3288bc51e2bb8da9ec8ab9d575d5c1cd2485
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2023-10-24 01:51:23 -0300

    commit

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ['echo', 'fortune', 'fortune2', 'help', 'reboot', 'update', 'widen']
+# https://stackoverflow.com/a/1057534/371160
+
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
